83b9f4cd355f876bfca134f963f1ab72db3a7378
徐晨凯 <xuchenkai_eb@163.com>
2021-10-21 17:20:02 +0800

    style(other): 重构

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,11 @@
 
 <style>
 #app {
+  margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
